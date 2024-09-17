37a3cf0ad12efe39eb95a9c39afaac9a1a40dfe2
Lucas Barbosa <lucasbrsa@users.noreply.github.com>
2024-09-17 14:39:57 +1000

    fixing W-W numbers from Juslins paper

--- fitting/tersoff.py
+++ fitting/tersoff.py
@@ -1,15 +1,15 @@
 import math 
 
-D0 = 6.64
-S = 2.96149
-beta = 1.80370
-r0 = 1.90547
+D0 = 5.41861
+S = 1.92708
+beta = 1.38528
+r0 = 2.34095
 
 l1 = beta * math.sqrt(2*S)
-A = D0/(S-1) * math.exp(l1 * r0)
+A = D0/(S-1) * math.exp(l1*r0)
 
 l2 = beta * math.sqrt(2/S)
-B = S*D0/(S - 1) * math.exp(l2 * r0)
+B = S*D0/(S - 1) * math.exp(l2*r0)
 
 print("l1 = " + str(l1))
 print("l2 = " + str(l2))
